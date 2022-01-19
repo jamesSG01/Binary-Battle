6cce3660e520fe316adae129d40f0affa863eb73
James Nguyen <minhkhoix01@gmail.com>
2022-01-19 04:43:56 -0800

    adding CSS

--- src/app/battle/chart-view/chart-view.component.ts
+++ src/app/battle/chart-view/chart-view.component.ts
@@ -15,23 +15,19 @@ export class ChartViewComponent implements OnInit {
     new TradingView.widget(
       {
       "autosize": true,
-      "symbol": "BINANCE:BTCPERP",
-      "interval": "1",
+      "symbol": "COINBASE:BTCUSD",
       "timezone": "Etc/UTC",
       "theme": "dark",
       "style": "1",
       "locale": "en",
       "toolbar_bg": "#f1f3f6",
       "enable_publishing": false,
+      "hide_legend": true,
+      "range": "1D",
       "save_image": false,
-      "studies": [
-        "BollingerBandsR@tv-basicstudies",
-        "StochasticRSI@tv-basicstudies",
-        "VWAP@tv-basicstudies"
-      ],
-      "container_id": "tradingview_af631"
-      }
-    );
+      "container_id": "tradingview_ae039"
+    }
+      );
   }
 
 }
