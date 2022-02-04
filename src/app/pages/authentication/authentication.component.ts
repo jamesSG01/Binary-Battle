dbbe8d83971032c62affe6736b491efad8360f11
James Nguyen <minhkhoix01@gmail.com>
2022-02-04 02:21:39 -0800

    Refine Authentication Page

--- src/app/pages/authentication/authentication.component.ts
+++ src/app/pages/authentication/authentication.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.css']
 })
 export class AuthenticationComponent implements OnInit {
-
+  isActive:boolean = false;
   constructor() { }
-
   ngOnInit(): void {
   }
-
+  activateRightPanel(){
+    this.isActive = !this.isActive;
+  };
 }
