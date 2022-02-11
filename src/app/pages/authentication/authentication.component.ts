1a4ac75eed8db7778a891255edda918e5607f511
James Nguyen <minhkhoix01@gmail.com>
2022-02-10 17:08:50 -0800

    Auth services

--- src/app/pages/authentication/authentication.component.ts
+++ src/app/pages/authentication/authentication.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service'
+import { LoginData } from 'src/app/shared/models/login-data.model'
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-authentication',
@@ -7,10 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthenticationComponent implements OnInit {
   isActive:boolean = false;
-  constructor() { }
+  constructor( 
+    private readonly authService: AuthService,
+    private readonly router: Router) { }
   ngOnInit(): void {
   }
   activateRightPanel(){
     this.isActive = !this.isActive;
   };
+  register(data: LoginData) {
+    this.authService
+      .register(data)
+      .then(() => this.router.navigate(['/login']))
+      .catch((e) => console.log(e.message));
+    alert('Registered!');
+  }
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,27 +1,27 @@
-import {
-  Auth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-} from '@angular/fire/auth';
-
 import { Injectable } from '@angular/core';
-import { LoginData } from '../interfaces/login-data.interface';
+import { Observable } from 'rxjs';
+import { Auth, 
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut
+ } from '@angular/fire/auth';
+ import { LoginData } from '../models/login-data.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private auth: Auth) {}
-
-  login({ username, password }: LoginData) {
-    return signInWithEmailAndPassword(this.auth, username, password);
+  /* Sign up */
+  register({ email, password }: LoginData) {
+    return createUserWithEmailAndPassword(this.auth, email, password);
   }
-
-  register({ username, password }: LoginData) {
-    return createUserWithEmailAndPassword(this.auth, username, password);
+  /* Sign in */
+  login({ email, password }: LoginData) {
+    return signInWithEmailAndPassword(this.auth, email, password);
   }
-
+  
+  /* Sign out */
   logout() {
     return signOut(this.auth);
   }
