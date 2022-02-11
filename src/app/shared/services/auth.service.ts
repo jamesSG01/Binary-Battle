13ffbd16a9121609c76cddbf42c39cb59458044f
James Nguyen <minhkhoix01@gmail.com>
2022-02-10 19:52:29 -0800

    Auth -> AngularFirestoreUID

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,17 +1,43 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
+import { getStorage, ref } from "firebase/storage";
+import {
+  Firestore, addDoc, collection, collectionData,
+  doc, docData, deleteDoc, updateDoc, DocumentReference, setDoc
+} from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 import { Auth, 
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   signOut
  } from '@angular/fire/auth';
- import { LoginData } from '../models/login-data.model';
+ import { AngularFireAuth } from '@angular/fire/compat/auth';
+ import { User } from '../models/users.model'; 
+ import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: Auth) {}
+  user$: Observable<User>;
+
+
+  constructor(private auth: Auth, private afAuth:AngularFireAuth, private afs: AngularFirestore, private router: Router){
+    // // Get the auth state, then fetch the Firestore user document or return null
+    // this.user$ = this.afAuth.authState.pipe(
+    //   switchMap(user => {
+    //       // Logged in
+    //     if (user) {
+    //       return this.afs.doc<User>('users/${user.uid}').valueChanges();
+    //     } else {
+    //       // Logged out
+    //       return of(null);
+    //     }
+    //   })
+    // )
+  }
+
   /* Sign up */
   register(email:string, password:string) {
     return createUserWithEmailAndPassword(this.auth, email, password);
@@ -21,7 +47,8 @@ export class AuthService {
     return signInWithEmailAndPassword(this.auth, email, password);
   }
   /* Sign out */
-  logout() {
-    return signOut(this.auth);
+  async logout() {
+    await this.afAuth.signOut();
+    this.router.navigate(['/']);
   }
 }
