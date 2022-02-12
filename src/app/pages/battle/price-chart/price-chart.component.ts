803d5f2ae8e45a6e6fa00caace049fe88d1b49d7
James Nguyen <minhkhoix01@gmail.com>
2022-02-11 16:36:44 -0800

    null

--- src/app/pages/battle/price-chart/price-chart.component.ts
+++ src/app/pages/battle/price-chart/price-chart.component.ts
@@ -30,45 +30,6 @@ export class PriceChartComponent implements OnInit {
       "container_id": "tradingview_ce365"
     }
       );
-    new TradingView.MediumWidget(
-      {
-      "symbols": [
-        [
-          "COINBASE:BTCUSD|1D"
-        ],
-        [
-          "FX:USDCAD|1D"
-        ],
-        [
-          "S&P",
-          "AMEX:SPY|1D"
-        ],
-        [
-          "OANDA:XAUUSD|1D"
-        ]
-      ],
-      "chartOnly": true,
-      "width": "900",
-      "height": "400",
-      "locale": "en",
-      "colorTheme": "dark",
-      "gridLineColor": "rgba(0, 0, 0, 0)",
-      "fontColor": "#787B86",
-      "isTransparent": true,
-      "autosize": false,
-      "showVolume": false,
-      "scalePosition": "no",
-      "scaleMode": "Normal",
-      "fontFamily": "Andale Mono, monospace",
-      "noTimeScale": false,
-      "valuesTracking": "1",
-      "chartType": "area",
-      "lineColor": "rgba(0, 255, 0, 1)",
-      "bottomColor": "rgba(0, 0, 0, 0)",
-      "topColor": "rgba(0, 255, 0, 0.3)",
-      "container_id": "tradingview_11397"
-    }
-    );
   }
 
 }
