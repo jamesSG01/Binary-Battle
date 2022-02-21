33daee106d53e2c89ac2435363f9358614a479a3
James Nguyen <minhkhoix01@gmail.com>
2022-02-20 23:35:30 -0800

    save
    - need to fix slider toggle to support pro mode & lightweight mode

--- src/app/pages/battle/price-chart/price-chart.component.ts
+++ src/app/pages/battle/price-chart/price-chart.component.ts
@@ -9,6 +9,7 @@ declare const TradingView: any;
   styleUrls: ['./price-chart.component.css']
 })
 export class PriceChartComponent implements OnInit {
+  proMode:boolean = false;
   hideBTC:boolean = true;
   hideETH:boolean = false;
   constructor() { }
@@ -50,7 +51,49 @@ export class PriceChartComponent implements OnInit {
       "container_id": "tradingview_adef8"
       }
     );
-    
+    //light weight
+    new TradingView.MediumWidget(
+      {
+      "symbols": [
+        [
+          "COINBASE:BTCUSD|1D"
+        ],
+        [
+          "FX:USDCAD|1D"
+        ],
+        [
+          "S&P",
+          "AMEX:SPY|1D"
+        ],
+        [
+          "OANDA:XAUUSD|1D"
+        ]
+      ],
+      "chartOnly": true,
+      "width": "100%",
+      "height": "600px",
+      "locale": "en",
+      "colorTheme": "dark",
+      "gridLineColor": "rgba(0, 0, 0, 0)",
+      "fontColor": "#787B86",
+      "isTransparent": true,
+      "autosize": true,
+      "showVolume": false,
+      "scalePosition": "no",
+      "scaleMode": "Normal",
+      "fontFamily": "Andale Mono, monospace",
+      "noTimeScale": false,
+      "valuesTracking": "1",
+      "chartType": "area",
+      "lineColor": "rgba(0, 255, 0, 1)",
+      "bottomColor": "rgba(0, 0, 0, 0)",
+      "topColor": "rgba(0, 255, 0, 0.3)",
+      "container_id": "tradingview_11397"
+    }
+    );
+  }
+  setProMode() {
+    this.proMode = !this.proMode;
   }
   show(ticker:string) {
     switch(ticker){
