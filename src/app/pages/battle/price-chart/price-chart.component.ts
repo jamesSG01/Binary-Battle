d9eca94c8b2b919896ae4ec7c17c016dcfe3a0f2
James Nguyen <minhkhoix01@gmail.com>
2022-02-20 22:52:25 -0800

    save--

--- src/app/pages/battle/price-chart/price-chart.component.ts
+++ src/app/pages/battle/price-chart/price-chart.component.ts
@@ -9,10 +9,12 @@ declare const TradingView: any;
   styleUrls: ['./price-chart.component.css']
 })
 export class PriceChartComponent implements OnInit {
-  
+  hideBTC:boolean = true;
+  hideETH:boolean = false;
   constructor() { }
   
   ngOnInit(): void {
+    //BTCUSD
     new TradingView.widget(
       {
       "autosize": true,
@@ -28,8 +30,39 @@ export class PriceChartComponent implements OnInit {
       "hide_legend": true,
       "save_image": false,
       "container_id": "tradingview_ce365"
-    }
-      );
+      }
+    );
+    //ETHUSD
+    new TradingView.widget(
+      {
+      "autosize": true,
+      "symbol": "COINBASE:ETHUSD",
+      "interval": "1",
+      "timezone": "Etc/UTC",
+      "theme": "dark",
+      "style": "1",
+      "locale": "en",
+      "toolbar_bg": "#f1f3f6",
+      "enable_publishing": false,
+      "hide_top_toolbar": true,
+      "hide_legend": true,
+      "save_image": false,
+      "container_id": "tradingview_adef8"
+      }
+    );
+    
   }
-
+  show(ticker:string) {
+    switch(ticker){
+      case 'BTC': {
+        this.hideBTC = false;
+        this.hideETH = true;
+        break;
+      } case 'ETH': {
+        this.hideETH = false;
+        this.hideBTC = true;
+        break;
+      }
+    }
+  }
 }
